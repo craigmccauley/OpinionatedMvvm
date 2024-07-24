b13dd178bb4033b890518cb72cc82efe6fe3f7fe
Craig McCauley <craig.mccauley@gmail.com>
2024-07-24 17:10:19 +0100

    switched from WPF-UI to ModernWpfUI, works but get binding error when settings is selected

--- src/MvvmApp.Wpf/Pages/NavPage.xaml.cs
+++ src/MvvmApp.Wpf/Pages/NavPage.xaml.cs
@@ -1,8 +1,4 @@
-﻿using MvvmApp.Core.Features.NavPage;
-using MvvmApp.Wpf.Infrastructure.Converters;
-using System.Windows;
-using System.Windows.Controls;
-using Wpf.Ui.Controls;
+﻿using System.Windows.Controls;
 
 //Sigh, MvvmApp.Wpf and Wpf.Ui namespaces get confused in the generated file.
 namespace MvvmAppWpf.Pages;
@@ -11,13 +7,5 @@ public partial class NavPage : UserControl
     public NavPage()
     {
         InitializeComponent();
-    }
-
-    private void NavigationView_Loaded(object sender, RoutedEventArgs e)
-    {
-        //var navigationView = (NavigationView)sender;
-        //var vm = (NavPageViewModel)DataContext;
-
-        //navigationView.Navigate(MenuItemConverter.MapToPage(vm.SelectedMenuItem.NavDestination), vm.SelectedView);
     }
 }
--- src/MvvmApp.Wpf/Program.cs
+++ src/MvvmApp.Wpf/Program.cs
@@ -3,8 +3,6 @@ using MvvmApp.Core.Features.MainPage;
 using MvvmApp.Core.Features.NavPage;
 using MvvmApp.Core.Infrastructure.Application;
 using MvvmApp.Wpf.Infrastructure.Application;
-using System.Resources;
-using System.Windows;
 
 namespace MvvmApp.Wpf;
 public class Program
@@ -18,22 +16,14 @@ public class Program
         pageViewModelGetterService!.Initialize(pageViewModelCreatorService);
 
         var app = new App();
+        app.InitializeComponent();
         app.Startup += (sender, e) =>
         {
             var mainPageViewModel = pageViewModelGetterService.GetPageViewModel(AppPages.MainPage) as MainPageViewModel;
             var navPageViewModel = pageViewModelGetterService.GetPageViewModel(AppPages.NavPage) as NavPageViewModel;
-            //var welcomePageViewModel = pageViewModelGetterService.GetPageViewModel(AppPages.WelcomePage) as WelcomePageViewModel;
 
             mainPageViewModel!.SelectedView = navPageViewModel!;
 
-            foreach (var uri in new[]
-            {
-                "pack://application:,,,/Wpf.Ui;component/Resources/Theme/Light.xaml",
-                "pack://application:,,,/Wpf.Ui;component/Resources/Wpf.Ui.xaml"
-            })
-            {
-                app.Resources.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri(uri) });
-            }
 
             app.MainWindow = new MainWindow
             {
